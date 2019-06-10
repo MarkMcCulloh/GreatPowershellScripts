e29b09f3831b10559668f4f2ddd976fac537d7b0
Mark McCulloh <Mark.McCulloh@gmail.com>
2019-06-10 13:15:15 -0400

    First script, may want to make use this more often

---
+++ GenerateServiceActivationsFromSvcs.ps1
@@ -0,0 +1,15 @@
+﻿param($SvcDirectory)
+
+cd $SvcDirectory
+
+$files = (Get-ChildItem).Name
+
+[regex]$regex = '\w+="[^"]+"'
+
+ foreach ($file in $files) {
+   foreach ($match in $regex.Matches((Get-Content $file)).Value){
+    Invoke-Expression ('$' + $match)
+   }
+
+   Write-Host "<add service=""$($Service), $($Name)"" relativeAddress=""tmp/$($file)"" />"
+ }
